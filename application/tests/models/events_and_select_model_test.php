3cbbbcc06be47c7ac745acb9810c4224c6a4cd08
k-kuwahara <kuwahara@lepra.jp>
2015-12-21 16:09:29 +0900

    query_modelのテストケース追加

--- application/tests/models/events_and_select_model_test.php
+++ application/tests/models/events_and_select_model_test.php
@@ -7,11 +7,11 @@ class Events_and_select_model_test extends TestCase {
         $this->CI =& get_instance();
 
         $this->CI->load->model('events_model');
-        $this->CI->load->model('join_model');
+        $this->CI->load->model('select_model');
         $this->CI->load->model('query_model');
 
         $this->eventsObj = $this->CI->events_model;
-        $this->joinObj = $this->CI->join_model;
+        $this->selectObj = $this->CI->select_model;
         $this->queryObj = $this->CI->query_model;
     }
 
@@ -19,26 +19,56 @@ class Events_and_select_model_test extends TestCase {
     public function test_index() {
         // 全イベントを削除
         $deleteResult = $this->queryObj->physicalDelete('dt_event');
+        // 正常終了確認
         $this->assertEquals($deleteResult, TRUE);
 
-        // データ取得
-        $eventResult = $this->eventsObj->getEvents();
+        // 登録データ取得
+        list($eventResult, $mess) = $this->eventsObj->getEvents();
+        // 正常終了確認
         $this->assertEquals(count($eventResult), 0);
+        $this->assertEquals($mess, "");
     }
 
-    // データ挿入周りのテスト
+    // データ挿入テスト
     public function test_data_insert() {
-        $params = [
-            'answer_date' => date('Y-m-d H:i'),
-            'joinResult'  => 1,
-            'joinName'    => '桑原',
-            'joinEmail'   => 'kuwahara@lepra.jp',
-            'joinMemo'    => '回答一つ目'
-        ];
+        $arrParams = array(
+            array(
+                'eventTitle' => 'テストイベントタイトル1',
+                'eventDate'  => '2015/12/22 19:00',
+                'adminEmail' => 'kuwahara@lepra.jp',
+            ),
+            array(
+                'eventTitle' => 'テストイベントタイトル2',
+                'eventDate'  => '2015/12/25 20:30',
+                'adminEmail' => 'dummy@test.jp',
+            ),
+            array(
+                'eventTitle' => 'テストイベントタイトル3',
+                'eventDate'  => '2016/01/04 09:30',
+                'adminEmail' => 'hogehoge@test.co.jp',
+            )
+        );
 
-        // 登録実行
-        list($result, $mess) = $this->joinObj->registerMember($params);
+        foreach ($arrParams as $vals) {
+            // 登録実行
+            list($result, $mess) = $this->selectObj->registerEvent($vals);
+            // 正常終了確認
+            $this->assertTrue($result);
+            $this->assertEquals($mess, "");
+        }
+
+        // 登録データ取得
+        list($eventResult, $mess) = $this->eventsObj->getEvents();
         // 正常終了確認
-        $this->assertEquals($result, TRUE);
+        $this->assertEquals(count($eventResult), 3);
+        $this->assertEquals($eventResult[0]['event_title'], 'テストイベントタイトル1');
+        $this->assertEquals($eventResult[1]['event_date'], '2015-12-25 20:30:00');
+        $this->assertEquals($eventResult[2]['email'], 'hogehoge@test.co.jp');
+        $this->assertEquals($mess, "");
+    }
+
+    // データ更新テスト
+    public function test_data_update() {
+
     }
 }
