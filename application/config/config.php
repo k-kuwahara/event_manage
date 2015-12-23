98a6183cc8cd8bd3b5b4b1144f2654e284323257
k-kuwahara <zensin0082@gmail.com>
2015-12-23 22:07:01 +0900

    base_urlの初期化

--- application/config/config.php
+++ application/config/config.php
@@ -17,8 +17,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://sche.lepra/';
-//$config['base_url'] = '';
+$config['base_url'] = '';
+
 /*
 |--------------------------------------------------------------------------
 | Index File
